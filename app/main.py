5a8a825a3905c34a79a7faf870a3f1ba97f20e0a
Sergei Nozovtsev <glenmetthews@gmail.com>
2024-02-21 01:43:22 +0200

    added 03 package roc

---
+++ app/main.py
@@ -0,0 +1,57 @@
+import time
+
+import websocket
+import json
+import re
+import rel
+import os
+import logging
+from websocket import WebSocket
+import sys
+
+import utils
+
+def on_open(ws: WebSocket):
+    request = {
+        "cmd": "auth_req",
+        "login": os.getenv('USER'),
+        "password": os.getenv('PASSWORD')
+    }
+
+    ws.send(json.dumps(request))
+
+
+def on_message(ws: WebSocket, message):
+    msg_json = json.loads(message)
+
+    if 'data' in msg_json:
+        if re.match(r'^03\w{8}$', msg_json["data"]) and utils.check_request_time(request_time=msg_json["data"][2:]):
+            utils.send_time_package(ws, dev_eui=msg_json['devEui'], request_time=msg_json["data"][2:])
+            logging.info(f"Время скорректировано. DevEui = {msg_json['devEui']}. Время на датчике было - {int(msg_json['data'][2:], 16)}")
+        if re.match(r'^03$', msg_json["data"]):
+            utils.send_time_package(ws, dev_eui=msg_json['devEui'])
+            logging.info(f"Время скорректировано. DevEui = {msg_json['devEui']}")
+
+
+def on_error(ws: WebSocket, error):
+    print(error)
+
+
+def on_close(ws: WebSocket):
+    print("Connection closed")
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s | %(asctime)s | %(message)s", stream=sys.stdout)
+
+    ws = websocket.WebSocketApp(
+        url=f"ws://{os.getenv('HOST')}:{os.getenv('PORT')}",
+        on_open=on_open,
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
+    )
+    ws.run_forever(dispatcher=rel, reconnect=5)
+    rel.signal(2, rel.abort)
+    rel.dispatch()
---
+++ app/utils.py
@@ -0,0 +1,44 @@
+import json
+import time
+import os
+
+
+def get_hex_timestamp():
+    now_timestamp = int(time.time())
+    hex_str = f'{now_timestamp:x}'
+    return hex_str
+
+
+def check_request_time(request_time: str):
+    return int(time.time()) - int(request_time, 16) > int(os.getenv('DELAY'))
+
+
+def generate_time_package(dev_eui: str, port: int = 60, ack: bool = False, request_time: str = str()):
+    return {
+        "cmd": "send_data_req",
+        "data_list": [
+            {
+                "devEui": dev_eui,
+                "data": f'03{get_hex_timestamp()}{request_time}',
+                "port": port,
+                "ack": ack
+            }
+        ]
+    }
+
+
+def send_time_package(ws, dev_eui, request_time: str = None):
+    if request_time:
+        response = generate_time_package(dev_eui=dev_eui, request_time=request_time)
+    else:
+        response = generate_time_package(dev_eui=dev_eui)
+    ws.send(json.dumps(response))
+
+
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    print(send_time_package(dev_eui='sss', request_time='65d514e3'))
